6db5ce22d5728d15429eefd879dba5e2d98580c2
jonpardev <jonpardev@gmail.com>
2022-11-23 20:47:47 +0000

    Deploying to release-server from @ jonpardev/tt-see@7effddafe55e95fabde474a630124eafaa5a3505 🚀

--- src/services/officialAlert.service.ts
+++ src/services/officialAlert.service.ts
@@ -112,7 +112,7 @@ const transformToAlerts = async () => {
  */
 const getOfficialRawRoutes = async() => {
     try {
-        const response = await officialApi.get<OfficialRaw>("/");
+        const response = await officialApi.get<OfficialRaw>("");
         const routes = response.data.routes;
         console.log(response);
         if (!routes) throw new Error(`[ERROR:getOfficialAlerts] Cannot find 'routes'`);
